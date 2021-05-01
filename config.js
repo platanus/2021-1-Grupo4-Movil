1d62de627dd3d2206241d58a155681c725623778
Luciano Davico <lldavico@uc.cl>
2021-05-01 14:26:15 -0400

    feat(app@config): add app config & fetching services

---
+++ config.js
@@ -0,0 +1,11 @@
+export default config = {
+    api: {
+        host: process.env.REACT_APP_API_HOST,
+        endpoints: {
+            users: {
+                createUser: 'api/v1/users/registrations',
+                logIn: 'api/v1/users/sessions',
+            },
+        }
+    },
+}
