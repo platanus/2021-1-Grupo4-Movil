018e28e87f98bb5f9feff0c3dafe689012662f31
jmsilvas <jmsilva10@uc.cl>
2021-05-17 14:52:29 -0400

    style(EditRecipeScreen): rename editRecipeScreen to EditRecipeSCreen

--- navigators/recipesStack.js
+++ navigators/recipesStack.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Recipes from '../screens/recipes/RecipesScreen';
 import Recipe from '../screens/recipes/SingleRecipeScreen';
-import editRecipe from '../screens/recipes/editRecipeScreen';
+import EditRecipe from '../screens/recipes/EditRecipeScreen';
 
 const MainRecipesStack = createStackNavigator();
 
@@ -11,7 +11,7 @@ function RecipesStackScreen() {
     <MainRecipesStack.Navigator initialRouteName="Recetas" >
       <MainRecipesStack.Screen name="Recetas" component={Recipes} />
       <MainRecipesStack.Screen name="Receta" component={Recipe} />
-      <MainRecipesStack.Screen name="Editar Receta" component={editRecipe} />
+      <MainRecipesStack.Screen name="Editar Receta" component={EditRecipe} />
     </MainRecipesStack.Navigator>
   );
 }
