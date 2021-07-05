6a17b029163760150d2bc2a1e711301add8974c8
Luciano Davico <lldavico@uc.cl>
2021-07-04 22:24:42 -0400

    fixing styles and text formatting for shoppingList

--- utils/listToClipboard.js
+++ utils/listToClipboard.js
@@ -1,22 +1,34 @@
 import {
-    Alert,
-  } from 'react-native';
+  Alert,
+} from 'react-native';
 import Clipboard from 'expo-clipboard';
 
 const formatListForClipboard = (shoppingList) => {
-    return 'Esto se pego desde expo'
-  }
-  
-  const copyList = (providers) => {
-    const listString = formatListForClipboard(providers)
-  
-    console.log(listString)
-    Clipboard.setString(listString)
-  
-    Alert.alert('¡Los datos han sido copiados con éxito!', '',
-      [{ text: 'Ok', style: 'default' }],
-    );
-  }
-  
+  let shoppingListString = '';
+
+  shoppingList.forEach((provider) => {
+    shoppingListString += `Compras en *${provider.provider}*:\n\n`;
+
+    let totalPrice = 0;
+    provider.ingredients.forEach((ingredient) => {
+      const totalPriceIngredient = ingredient.quantity * ingredient.total_price;
+      totalPrice += totalPriceIngredient;
+      shoppingListString += `${ingredient.name} (${ingredient.quantity}): $${totalPriceIngredient}\n`;
+    });
+    shoppingListString += `*Precio total: $${totalPrice}*\n\n`;
+  });
+
+  return shoppingListString;
+};
+
+const copyList = (providers) => {
+  const listString = formatListForClipboard(providers);
+
+  Clipboard.setString(listString);
+
+  Alert.alert('¡Los datos han sido copiados con éxito!', '',
+    [{ text: 'Ok', style: 'default' }],
+  );
+};
 
 export default copyList;
