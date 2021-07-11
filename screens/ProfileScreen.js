4e9ccc58945ee9a68796e21fed882a45dc205987
jmsilvas <jmsilva10@uc.cl>
2021-07-10 20:04:54 -0400

    feat(ProfileScreen); create profile view with UI and api connection

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,21 +1,129 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, TextInput, Alert } from 'react-native';
 import { useStoreActions } from 'easy-peasy';
-import styles from '../styles/authStyles';
+import Icon from 'react-native-vector-icons/Ionicons';
+import styles from '../styles/Profile/profileStyles';
+import colors from '../styles/appColors';
+import KeyboardAvoidWrapper from '../components/KeyboardAvoidWrapper';
 
 function Profile() {
   const logout = useStoreActions((actions) => actions.setLogOut);
+  const changePassword = useStoreActions((actions) => actions.changePassword);
+  const [oldPassword, setOldPassword] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [hideOldPassword, setHideOldPassword] = useState(true);
+  const [hideNewPasswords, setHideNewPasswords] = useState(true);
+
+  function handleChangePassword() {
+    if (!oldPassword || !password || !passwordConfirmation) {
+      Alert.alert('Debes llenar todos los campos');
+
+      return;
+    }
+    if (password !== passwordConfirmation) {
+      Alert.alert('Contraseñas no coinciden');
+
+      return;
+    }
+
+    const body = {
+      user: {
+        oldPassword,
+        password,
+        passwordConfirmation,
+      },
+    };
+
+    changePassword(body)
+      .then(() => {
+        Alert.alert('¡La clave se cambió con éxito!', '',
+          [{ text: 'Ok', style: 'default' }],
+        );
+        setOldPassword('');
+        setPassword('');
+        setPasswordConfirmation('');
+      })
+      .catch(() => {
+        Alert.alert('Hubo un error al cambiar la contraseña', '¿Ingresaste bien la contraseña?');
+      });
+  }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.logContainer}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => logout()}>
-          <Text style={styles.buttonText}>Cerrar Sesión</Text>
-        </TouchableOpacity>
+    <KeyboardAvoidWrapper>
+      <View style={styles.container}>
+        <Icon name={'ios-person-circle'}
+          color={colors.kitchengramGray800}
+          size={60}
+        />
+        <Text style={styles.profileTitle}>
+          Perfil de usuario
+        </Text>
+        <View style={styles.changePasswordContainer}>
+          <Text style={styles.sectionTitle}>
+            Modifica tus datos
+          </Text>
+          <Text style={styles.loginText}>
+            Contraseña:
+          </Text>
+          <View>
+            <TextInput
+              value={oldPassword}
+              onChangeText={(pass) => setOldPassword(pass)}
+              secureTextEntry={hideOldPassword}
+              style={styles.input}
+            />
+            <Icon name={hideOldPassword ? 'eye-off-sharp' : 'eye-sharp'}
+              color={colors.kitchengramGray600}
+              size={20} style={styles.eye}
+              onPress={() => setHideOldPassword(!hideOldPassword)}/>
+          </View>
+          <Text style={styles.loginText}>
+            Nueva contraseña:
+          </Text>
+          <View>
+            <TextInput
+              value={password}
+              onChangeText={(pass) => setPassword(pass)}
+              secureTextEntry={hideNewPasswords}
+              style={styles.input}
+            />
+            <Icon name={hideNewPasswords ? 'eye-off-sharp' : 'eye-sharp'}
+              color={colors.kitchengramGray600}
+              size={20} style={styles.eye}
+              onPress={() => setHideNewPasswords(!hideNewPasswords)}/>
+          </View>
+          <Text style={styles.loginText}>
+            Confirmar contraseña:
+          </Text>
+          <View>
+            <TextInput
+              value={passwordConfirmation}
+              onChangeText={(pass) => setPasswordConfirmation(pass)}
+              secureTextEntry={hideNewPasswords}
+              style={styles.input}
+            />
+            <Icon name={hideNewPasswords ? 'eye-off-sharp' : 'eye-sharp'}
+              color={colors.kitchengramGray600}
+              size={20}
+              style={styles.eye}
+              onPress={() => setHideNewPasswords(!hideNewPasswords)}/>
+          </View>
+          <View style={styles.buttonsContainer}>
+            <TouchableOpacity
+              style={styles.sessionButton}
+              onPress={() => logout()}>
+              <Text style={styles.buttonsText}>Cerrar sesión</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.saveButton}
+              onPress={handleChangePassword}>
+              <Text style={styles.buttonsText}>Guardar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
-    </View>
+    </KeyboardAvoidWrapper>
   );
 }
 
---
+++ styles/Profile/profileStyles.js
@@ -0,0 +1,101 @@
+import { StyleSheet } from 'react-native';
+import colors from '../appColors';
+
+const styles = StyleSheet.create({
+
+  container: {
+    backgroundColor: colors.kitchengramWhite,
+    minHeight: '100%',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: '20%',
+    flex: 1,
+    paddingHorizontal: '5%',
+  },
+
+  profileTitle: {
+    color: colors.kitchengramGray800,
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    paddingLeft: 5,
+  },
+
+  changePasswordContainer: {
+    height: 400,
+    width: '100%',
+    justifyContent: 'flex-start',
+    paddingHorizontal: '5%',
+  },
+
+  sectionTitle: {
+    color: colors.kitchengramBlack,
+    fontSize: 18,
+    marginVertical: 15,
+    paddingLeft: 5,
+  },
+
+  loginText: {
+    paddingLeft: 5,
+    marginTop: 5,
+    fontSize: 18,
+    color: colors.kitchengramGray600,
+  },
+
+  input: {
+    height: 50,
+    margin: 5,
+    borderWidth: 2,
+    borderColor: colors.kitchengramGray600,
+    maxWidth: '100%',
+    marginLeft: 3,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+  },
+
+  eye: {
+    position: 'absolute',
+    top: '30%',
+    right: '5%',
+  },
+
+  buttonsContainer: {
+    width: '100%',
+    height: 75,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    marginHorizontal: '5%',
+    paddingVertical: '5%',
+  },
+
+  sessionButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '48%',
+    height: '100%',
+    borderRadius: 5,
+    backgroundColor: colors.kitchengramGray800,
+  },
+
+  saveButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '48%',
+    height: '100%',
+    borderRadius: 5,
+    backgroundColor: colors.kitchengramGreen500,
+  },
+
+  buttonsText: {
+    color: colors.kitchengramWhite,
+    fontSize: 16,
+  },
+
+});
+
+export default styles;
